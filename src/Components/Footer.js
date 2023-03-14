eb2c2292ea43665bd2bdf6bb7d7eaee8bee5fb3d
Salonipatil777 <116019571+Salonipatil777@users.noreply.github.com>
2023-03-14 10:52:45 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,30 +1,19 @@
-import React from "react";
-import {
-  FacebookIcon,
-  FacebookShareButton,
-  LinkedinShareButton,
-  LinkedinIcon,
-  TwitterShareButton,
-  TwitterIcon,
-  WhatsappIcon,
-  WhatsappShareButton,
-} from "react-share";
-import plusblue from "../images/shap/plus-blue.png";
-import logo from "../images/Raj-hospital.png";
-import footer from "../images/background/footer.jpg";
-// import twitter from "../images/social/twitter.png";
-import instagram from "../images/social/instagram.png";
-// import linkedin from "../images/social/linkedin.png";
-// import facebook from "../images/social/facebook.png";
-import waveBlue from "../images/shap/wave-blue.png";
-import circledots from "../images/shap/circle-dots.png";
+import React from 'react'
+import plusblue from '../images/shap/plus-blue.png'
+import logo from '../images/Raj-hospital.png'
+import footer from '../images/background/footer.jpg'
+import twitter from '../images/social/twitter.png'
+import instagram from '../images/social/instagram.png'
+import linkedin from '../images/social/linkedin.png'
+import facebook from '../images/social/facebook.png'
+import waveBlue from '../images/shap/wave-blue.png'
+import circledots from '../images/shap/circle-dots.png'
 
 const Footer = () => {
-  const shareUrl = "https://raj-hospital.vercel.app/";
   return (
     <>
-      {/* <!-- Footer ==== --> */}
-      <footer className="footer" style={{ backgroundImage: { footer } }}>
+     {/* <!-- Footer ==== --> */}
+     <footer className="footer" style={{backgroundImage: {footer}}}>
         {/* <!-- Footer Top --> */}
         <div className="footer-top">
           <div className="container">
@@ -32,23 +21,19 @@ const Footer = () => {
               <div className="col-xl-3 col-lg-3 col-md-6">
                 <div className="widget widget_info">
                   <div className="footer-logo">
-                    <a href="index.html">
-                      <img src={logo} alt="" />
-                    </a>
+                    <a href="index.html"><img src={logo} alt="" /></a>
                   </div>
                   <div className="ft-contact">
                     <p>
-                      <ul>
-                        <li>Find A Doctor</li>
-                        <li>Service Excellence</li>
-                        <li>Health Check Ups</li>
-                        <li> Patient Testimonials</li>
-                      </ul>
+                    <ul>
+                      <li>Find A Doctor</li>
+                      <li>Service Excellence</li>
+                      <li>Health Check Ups</li>
+                      <li> Patient Testimonials</li>
+                    </ul>
                     </p>
                     <div className="contact-bx">
-                      <div className="icon">
-                        <i className="fas fa-phone-alt"></i>
-                      </div>
+                      <div className="icon"><i className="fas fa-phone-alt"></i></div>
                       <div className="contact-number">
                         <span>Contact Us</span>
                         <h4 className="number">+(91)9112666699</h4>
@@ -62,34 +47,22 @@ const Footer = () => {
                   <h3 className="footer-title">Quick Links</h3>
                   <ul>
                     <li>
-                      <a href="about-us.html">
-                        <span>About Us</span>
-                      </a>
+                      <a href="about-us.html"><span>About Us</span></a>
                     </li>
                     <li>
-                      <a href="services.html">
-                        <span>Services</span>
-                      </a>
+                      <a href="services.html"><span>Services</span></a>
                     </li>
                     <li>
-                      <a href="booking.html">
-                        <span>Booking</span>
-                      </a>
+                      <a href="booking.html"><span>Booking</span></a>
                     </li>
                     <li>
-                      <a href="faq.html">
-                        <span>FAQ</span>
-                      </a>
+                      <a href="faq.html"><span>FAQ</span></a>
                     </li>
                     <li>
-                      <a href="blog-grid.html">
-                        <span>Blogs</span>
-                      </a>
+                      <a href="blog-grid.html"><span>Blogs</span></a>
                     </li>
                     <li>
-                      <a href="team.html">
-                        <span>Our Team</span>
-                      </a>
+                      <a href="team.html"><span>Our Team</span></a>
                     </li>
                   </ul>
                 </div>
@@ -99,34 +72,22 @@ const Footer = () => {
                   <h3 className="footer-title">Our Service</h3>
                   <ul>
                     <li>
-                      <a href="service-detail.html">
-                        <span>Dental Care</span>
-                      </a>
+                      <a href="service-detail.html"><span>Dental Care</span></a>
                     </li>
                     <li>
-                      <a href="service-detail.html">
-                        <span>Cardiac Clinic</span>
-                      </a>
+                      <a href="service-detail.html"><span>Cardiac Clinic</span></a>
                     </li>
                     <li>
-                      <a href="service-detail.html">
-                        <span>Massege Therapy</span>
-                      </a>
+                      <a href="service-detail.html"><span>Massege Therapy</span></a>
                     </li>
                     <li>
-                      <a href="service-detail.html">
-                        <span>Cardiology</span>
-                      </a>
+                      <a href="service-detail.html"><span>Cardiology</span></a>
                     </li>
                     <li>
-                      <a href="service-detail.html">
-                        <span>Precise Diagnosis</span>
-                      </a>
+                      <a href="service-detail.html"><span>Precise Diagnosis</span></a>
                     </li>
                     <li>
-                      <a href="service-detail.html">
-                        <span>Ambulance Services</span>
-                      </a>
+                      <a href="service-detail.html"><span>Ambulance Services</span></a>
                     </li>
                   </ul>
                 </div>
@@ -134,72 +95,34 @@ const Footer = () => {
               <div className="col-xl-3 col-lg-3 col-md-6">
                 <div className="widget widget_form">
                   <h3 className="footer-title">Subscribe</h3>
-                  <form
-                    className="subscribe-form subscription-form mb-30"
-                    action="https://meditro.themetrades.com/html/demo/script/mailchamp.php"
-                    method="post"
-                  >
+                  <form className="subscribe-form subscription-form mb-30"
+                    action="https://meditro.themetrades.com/html/demo/script/mailchamp.php" method="post">
                     <div className="ajax-message"></div>
                     <div className="input-group">
-                      <input
-                        name="email"
-                        required="required"
-                        className="form-control"
-                        placeholder="Email Address"
-                        type="email"
-                      />
+                      <input name="email" required="required" className="form-control" placeholder="Email Address"
+                        type="email" />
                     </div>
-                    <button
-                      name="submit"
-                      value="Submit"
-                      type="submit"
-                      className="btn btn-secondary shadow w-100"
-                    >
+                    <button name="submit" value="Submit" type="submit" className="btn btn-secondary shadow w-100">
                       Subscribe Now
                     </button>
                   </form>
                   <div className="footer-social-link">
                     <ul>
                       <li>
-                        <FacebookShareButton
-                          url={shareUrl}
-                          quote="Raj-hospital"
-                          hashtag="#RajHospital"
-                        >
-                          <FacebookIcon size={30} round={true} />
-                        </FacebookShareButton>
+                        <a target="blank" href="https://www.facebook.com/"><img src={facebook}
+                            alt="" /></a>
                       </li>
                       <li>
-                        <TwitterShareButton
-                          url={shareUrl}
-                          quote="Raj-hospital"
-                          hashtag="#RajHospital"
-                        >
-                          <TwitterIcon size={30} round={true} />
-                        </TwitterShareButton>
+                        <a target="blank" href="https://twitter.com/"><img src={twitter}
+                            alt="" /></a>
                       </li>
                       <li>
-                        <a target="blank" href="https://www.instagram.com/">
-                          <img src={instagram} alt="" />
-                        </a>
+                        <a target="blank" href="https://www.instagram.com/"><img src={instagram}
+                            alt="" /></a>
                       </li>
                       <li>
-                        <LinkedinShareButton
-                          url={shareUrl}
-                          quote="Raj-hospital"
-                          hashtag="#RajHospital"
-                        >
-                          <LinkedinIcon size={30} round={true} />
-                        </LinkedinShareButton>
-                      </li>
-                      <li>
-                        <WhatsappShareButton
-                          url={shareUrl}
-                          quote="Raj-hospital"
-                          hashtag="#RajHospital"
-                        >
-                          <WhatsappIcon size={30} round={true} />
-                        </WhatsappShareButton>
+                        <a target="blank" href="https://www.linkedin.com/"><img src={linkedin}
+                            alt="" /></a>
                       </li>
                     </ul>
                   </div>
@@ -214,8 +137,7 @@ const Footer = () => {
             <div className="row">
               <div className="col-12 text-center">
                 <p className="copyright-text">
-                  Copyright © 2023 Raj Hospitals Enterprise Ltd. All Rights
-                  Reserved.Developed by
+                  Copyright © 2023 Raj Hospitals Enterprise Ltd. All Rights Reserved.Developed by
                   <a href="https://upconn.in/">UPCONN</a>
                 </p>
               </div>
@@ -230,7 +152,7 @@ const Footer = () => {
       </footer>
       {/* <!-- Footer END ==== --> */}
     </>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
