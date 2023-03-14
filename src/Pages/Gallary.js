878e105b6711b05530635e5bb496d316d351f6e2
Salonipatil777 <116019571+Salonipatil777@users.noreply.github.com>
2023-03-14 11:54:08 +0530

    Update Gallary.js

--- src/Pages/Gallary.js
+++ src/Pages/Gallary.js
@@ -29,11 +29,37 @@ import g22 from "../images/gallary/g22.jpg";
 import g23 from "../images/gallary/g23.jpg";
 import g24 from "../images/gallary/g24.jpg";
 import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 const Gallary = () => {
+  const lightbox = document.createElement("div");
+  lightbox.id = "lightbox";
+  document.body.appendChild(lightbox);
+
+  const images = document.querySelectorAll("img");
+  images.forEach((image) => {
+    image.addEventListener("click", (e) => {
+      lightbox.classList.add("active");
+      const img = document.createElement("img");
+      img.src = image.src;
+      while (lightbox.firstChild) {
+        lightbox.removeChild(lightbox.firstChild);
+      }
+      lightbox.appendChild(img);
+    });
+  });
+
+  lightbox.addEventListener("click", (e) => {
+    if (e.target !== e.currentTarget) return;
+    lightbox.classList.remove("active");
+  });
   return (
     <>
       <div className="page-content bg-white">
+      <Helmet>
+          <title>Explore Our Gallary</title>
+          <meta name="description" content="Moments in doctors life."/>
+        </Helmet>
         {/* <!-- Inner Banner --> */}
         <div className="banner-wraper">
           <div
