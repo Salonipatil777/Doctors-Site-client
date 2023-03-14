44f779e6a53f331ea9f68354db5bb0663b9420c0
Salonipatil777 <116019571+Salonipatil777@users.noreply.github.com>
2023-03-14 11:52:50 +0530

    Update ContactList.js

--- src/Pages/ContactList.js
+++ src/Pages/ContactList.js
@@ -14,6 +14,7 @@ import waveBlue from "../images/shap/wave-blue.png";
 import circledots from "../images/shap/circle-dots.png";
 import img1 from "../images/banner/img1.jpg";
 import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 const ContactList = () => {
   const dispatch = useDispatch();
@@ -66,6 +67,11 @@ const ContactList = () => {
   return (
     <section>
       <div className="banner-wraper">
+      <Helmet>
+          <title>View Contacts
+          </title>
+          <meta name="description" content="Admin only"/>
+        </Helmet>
         <div
           className="page-banner"
           style={{ backgroundImage: `url(${img1})` }}
